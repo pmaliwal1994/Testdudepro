7c728852302d7c62a3d4fd8f5d664a384c9e280b
Pushpendra <pmaliwal.1994@gmail.com>
2025-05-16 17:35:49 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+#task 1
+num1=int(input("Enter 1st num "))
+num2=int(input("Enter 2nd num "))
+
+print("Addition",num1+num2)
+print("subtraction",num1-num2)
+print("Multiplication",num1*num2)
+if num2 <=0:
+    print("can not divide by 0 or less")
+else:
+    print("Divide", num1 / num2)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+s1=input("First name ")
+s2=input("last name ")
+print("Hello",s1,s2,"Welcome to world")
